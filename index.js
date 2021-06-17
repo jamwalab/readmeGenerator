3b85db0c80b8d54c949e8543519efb410133cdf9
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-06-17 02:51:55 -0400

    readme questions added

--- index.js
+++ index.js
@@ -85,40 +85,114 @@ const questions = [
                 return false;
             }
         }
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Please select optional data you want to add?",
+        choices: [
+            "MIT", "Apache","GNU", "ISC", "Rust"
+        ],
+        default: "MIT"
+    },
+    {
+        type: "confirm",
+        name: "videoOption",
+        message: "Would you like to add a video link?",
+        default: false
+    },
+    {
+        type: "input",
+        name: "video",
+        message: "Please enter the video link for your project!!",
+        when: ({videoOption}) => {
+            if (videoOption) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: input => {
+            if (input) {
+                return true;
+            }
+            else {
+                console.log("Link cannot be blank, please try again!!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "confirm",
+        name: "creditOption",
+        message: "Would you like to add credits for your project?",
+        default: false
+    },
+    {
+        type: "input",
+        name: "credit",
+        message: "Please enter credits for your project!!",
+        when: ({creditOption}) => {
+            if (creditOption) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: input => {
+            if (input) {
+                return true;
+            }
+            else {
+                console.log("Entry cannot be blank, please try again!!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "confirm",
+        name: "featureOption",
+        message: "Would you like to add features for your project?",
+        default: false
+    },
+    {
+        type: "input",
+        name: "feature",
+        message: "Please enter features for your project!!",
+        when: ({creditOption}) => {
+            if (creditOption) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: input => {
+            if (input) {
+                return true;
+            }
+            else {
+                console.log("Entry cannot be blank, please try again!!");
+                return false;
+            }
+        }
     }
 ]
 
-/*() => {
-    return inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "title",
-            message: "Please enter the title of your project!!",
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                }
-                else {
-                    console.log("Title cannot be blank, please try again!!");
-                    return false;
-                }
-            }
-        }
-    ])
-};*/
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
+    
     return inquirer
         .prompt(questions);
 };
 
 // Function call to initialize app
 init()
+    //.then(optionalData)
     .then(readmeData => {
         console.log(readmeData);
     });
