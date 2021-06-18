a9028a224957383059b4f3745e58a23ecd527865
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-06-18 03:11:26 -0400

    text generated till test section, screnshot option is added

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(data) {
-  if (data.addBadge === true) {
-    if (data.license === MIT) {
+function renderLicenseBadge(license, optional) {
+  if (optional.badgeOption) {
+    if (license === "MIT") {
       return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
     }
-    else if (data.license === Apache) {
+    else if (license === "Apache") {
       return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
     }
-    else if (data.license === GNU) {
+    else if (license === "GNU") {
       return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
     }
-    else if (data.license === ISC) {
+    else if (license === "ISC") {
       return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
     }
-    else if (data.license === EPL) {
+    else if (license === "EPL") {
       return "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
     }
   }
@@ -37,6 +37,20 @@ const tableOfContent = (installation, usage, contribution, test, license, option
   }
   return table;
 }
+
+const checkScreenshot = (optional, section) => {
+  console.log(optional)
+  if (optional.screenshot.length === 0) {
+    return "";
+  }
+  let ssText = "";
+  optional.screenshot.forEach(screenshot => {
+    if (screenshot.screenshotSection === section) {
+      ssText = ssText.concat(`\n![image](./assets/img/${screenshot.screenshotFile})`)
+    }
+  });
+  return ssText;
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
@@ -53,9 +67,32 @@ function generateMarkdown(data) {
 ## Description
 
 ${description}
+${checkScreenshot(optional, "Description")}
 
 ## Table of Contents
 ${tableOfContent(installation, usage, contribution, test, license, optional)}
+
+## License
+
+${license} ${renderLicenseBadge(license, optional)}
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+${checkScreenshot(optional, "Usage")}
+
+## Contribution
+
+${contribution}
+
+## Test
+
+${test}
+
 `;
 }
 
